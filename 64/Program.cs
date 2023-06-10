01ffdb88ec4a8e827e294e4c2267eb7267071173
irina shkola <iri6kass@mail.ru>
2023-06-10 20:36:38 +0300

    Задание 9

---
+++ 64/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 64: Задайте значения N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5. -> "5, 4, 3, 2, 1"
+// N = 8. -> "8, 7, 6, 5, 4, 3, 2, 1"
+void NatralToLow(int num, int count)
+{
+    if (count>num)
+    {
+        return;
+    }
+    else
+    {
+        NatralToLow(num, count+1);
+        Console.Write(count + " ");
+    }
+}
+
+System.Console.WriteLine("Введите число: ");
+int num  = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NatralToLow(num,  count);
